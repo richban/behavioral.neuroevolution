756b4e1d9e583706bfcad35a48031a178df6d233
Richard <rbanyi@me.com>
2018-10-11 18:47:43 +0200

    communication test with arduino/serial

--- zumo/python.arduino.py
+++ zumo/python.arduino.py
@@ -8,29 +8,44 @@ PORT = '/dev/cu.usbmodem14101'
 BAUDRATE = 9600
 RUNTIME = 10
 
-try:
-    ser = serial.Serial(PORT, BAUDRATE)
-    print('Opening Port name', ser.name)
-except serial.SerialException as e:
-    sys.exit(e)
 
-now = datetime.now()
+class Com(object):
+    def __init__(self, port=PORT, baudrate=BAUDRATE):
+        try:
+            self.ser = serial.Serial(PORT, BAUDRATE)
+        except serial.SerialException as e:
+            sys.exit(e)
 
-print('Program Starting...')
-time.sleep(1)
+    def __del__(self):
+        self.ser.close()
 
-while datetime.now() - now < timedelta(seconds=RUNTIME):
-    packet = ser.readline()
-    print("packets received: ", packet)
-    ser.write(str.encode('W, 100, 100\n'))
-    time.sleep(0.1)
-    ser.write(str.encode('B\n'))
+    def test_motor(self):
+        self.ser.write(str.encode('W, 100, 100\n'))
+        time.sleep(2)
+        self.ser.write(str.encode('B\n'))
+        time.sleep(2)
+        self.ser.write(str.encode('F\n'))
+        time.sleep(2)
+        self.ser.write(str.encode('R\n'))
+
+    def test_sensors(self):
+        now = datetime.now()
+        while datetime.now() - now < timedelta(seconds=RUNTIME):
+            self.ser.write(str.encode('S\n'))
+            time.sleep(0.1)
+            packet = self.ser.readline()
+            print("packets received: ", packet)
+
+    def stream_sensors(self):
+        while True:
+            packet = self.ser.readline()
+            time.slep(0.1)
+            print(packet)
 
 
-print('Shuting down the agent.')
-
-ser.write(str.encode('R\n'))
-time.sleep(0.5)
-ser.close()
-
-print('Agent shut down.')
+if __name__ == "__main__":
+    args = sys.argv[1:]
+    arduino = Com(*args)
+    # arduino.test_motor()
+    arduino.test_sensors()
+    # arduino.stream_sensors()
